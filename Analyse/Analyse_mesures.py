148e99b9d867e2eee52783384147ef3d19a34e33
paplebat <114826710+paplebat@users.noreply.github.com>
2022-11-05 22:03:10 +0100

    Update Analyse_mesures.py

--- Analyse/Analyse_mesures.py
+++ Analyse/Analyse_mesures.py
@@ -4,6 +4,7 @@ import numpy as np
 from random import gauss
 from recuperation_mesures import*
 from regression import*
+import pandas as pd
 
 def fusion(T,P):
     L=[]
@@ -37,49 +38,143 @@ def max_i(L):
             max=L[i]
             i_max=i
     return i_max
-
+def max_i2(L):
+    i_max=0
+    max=L[0]
+    for i in range(len(L)):
+        if L[i]>=max:
+            max=L[i]
+            i_max=i
+    return i_max
 
 '''On range les liste par ordre croissants selon K'''
+K,Q=getdata('all')
 M=tri_fusion(K, Q)
-K1=M[0][:-2]
-Q1=M[1][:-2]
+
+
+'''fontion pour nettoyer les données'''
+
+def analyse(Kf,Qf):
+    n=len(Kf)%10
+    
+    if n!=0:
+        Kf=Kf[:-n]
+        Qf=Qf[:-n]
+
+    MQ=[]
+    MK=[]
+    for i in range(len(Kf)//200):
+        a=[]
+        b=[]
+        for j in range(200):
+            a.append(Kf[j+i*200])
+            b.append(Qf[j+i*200])
+            
+        da=pd.Series(a,dtype='int64')
+        db=pd.Series(b,dtype='int64')
+        da.fillna(0)
+        db.fillna(0)
+        MK.append(np.mean(da))
+        MQ.append(np.mean(db))
+        '''dmk=pd.Series(MK)
+        dmq=pd.Series(MQ)
+        dmk.fillna(0)
+        dmq.fillna(0)'''
+
+
+
+    Kfin=[]
+    Qfin=[]
+    for i in range(len(Kf)//200):
+        A=[]
+        B=[]
+        for j in range(len(Kf)):
+            if Qf[j]>=0.8*MQ[i] and Qf[j]<=1.2*MQ[i]:
+                if Kf[j]>=0.9*MK[i] and Kf[j]<=1.1*MK[i]:
+                    A.append(Kf[j])
+                    Kfin.append(Kf[j])
+                    B.append(Qf[j])
+                    Qfin.append(Qf[j])
+
+        da=pd.Series(A,dtype='int64')
+        db=pd.Series(B,dtype='int64')
+        da.fillna(0)
+        db.fillna(0)
+        MK[i]=np.mean(da)
+        MQ[i]=np.mean(db)
+        '''dmk=pd.Series(MK)
+        dmq=pd.Series(MQ)
+        dmk.fillna(0)
+        dmq.fillna(0)'''
+    return Kfin, Qfin,MK,MQ
 
 '''On étudie la partie fluide, donc avant Q max qui est la capacité de la section (f pour fluide)'''
-C_i=max_i(Q1)
-Kf=K1[:C_i]
-Qf=Q1[:C_i]
+        
+C_i=max_i(M[1])
+Kf1,Qf1,MKf,MQf=analyse(M[0][:C_i],M[1][:C_i])
 
-n=len(Kf)%10
+'''On étudie la partie congestionnée, donc après Q max qui est la capacité de la section (c pour congestionnée)'''
 
-if n!=0:
-    Kf=Kf[:-n]
-    Qf=Qf[:-n]
-MQ=[]
-MK=[]
-for i in range(len(Kf)//20):
-    a=0
-    b=0
-    for j in range(20):
-        a+=Kf[j+i*20]
-        b+=Qf[j+i*20]
-    MK.append(a/20)
-    MQ.append(b/20)
+C_i=max_i2(M[1])
+Kc,Qc,MKc,MQc=analyse(M[0][C_i:],M[1][C_i:])
+
+
+'''plot'''
+
+plt.plot(K, Q, '+')  
+plt.title('Diagramme Fondamental')
+plt.xlabel('Concentration (veh/km)')
+plt.ylabel('Débit (veh/h)')
+
+
+'''plot pour partie fluide'''     
+        
+a,b=np.polyfit(Kf1,Qf1,1)
+x=np.linspace(0,max(Kf1),1000)
+y=a*x+b
+plt.plot(Kf1, Qf1, '+')
+plt.plot(MKf, MQf)
+plt.plot(x,y)
+
+
+'''plot pour partie congestionnée'''
+
+a,b=np.polyfit(Kc,Qc,1)
+x=np.linspace(max(Kf1),max(Kc),1000)
+y=a*x+b
+plt.plot(Kc, Qc, '+')
+plt.plot(MKc, MQc)
+plt.plot(x,y)
+
+plt.show()
+''''''
+
+
+'''
+print(len(M[0]))
+plt.plot(M[0],M[1],'+')
+plt.show()
+'''
+
+
+'''
+
+a,b=np.polyfit(Kf, Qf,1)
 Kfin=[]
 Qfin=[]
-for i in range(len(Kf)//20):
-    A=[]
-    B=[]
-    for j in range(len(Kf)):
-        if Qf[j]>=0.8*MQ[i] and Qf[j]<=1.2*MQ[i]:
-            if Kf[j]>=0.9*MK[i] and Kf[j]<=1.1*MK[i]:
-                A.append(Kf[j])
-                Kfin.append(Kf[j])
-                B.append(Qf[j])
-                Qfin.append(Qf[j])
-    MK[i]=np.mean(A)
-    MQ[i]=np.mean(B)           
- 
-plt.plot(Kf, Qf, '+')   
-plt.plot(MK, MQ)
-plt.plot(Kfin, Qfin, '+')
+for j in range(1,11):
+    Kfin=[]
+    Qfin=[]
+    for i in range(len(Kf)):
+        if Qf[i]>=(1-0.5/j)*(a*Kf[i]+b) and Qf[i]<=(1+0.5/j)*(a*Kf[i]+b):
+            Kfin.append(Kf[i])
+            Qfin.append(Qf[i])
+            a,b=np.polyfit(Kfin, Qfin,1)
+x=np.linspace(0,50,1000)
+y=a*x+b
+print(len(Kfin))
+print(a,b)
+plt.plot(Kfin, Qfin)
+plt.plot(x,y)
 plt.show()
+'''
