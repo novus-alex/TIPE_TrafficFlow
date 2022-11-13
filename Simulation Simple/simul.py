233bfdefa79d25d0230e4a410482ae3689274066
Alex <alexandrehachet1@gmail.com>
2022-11-13 19:09:27 +0100

    adding pid controller for vehicles

--- Simulation Simple/simul.py
+++ Simulation Simple/simul.py
@@ -1,5 +1,7 @@
 import pygame
 from math import *
+import matplotlib.pyplot as plt
+import time
 
 class Screen:
     def __init__(self):
@@ -8,32 +10,34 @@ class Screen:
         self.scr = pygame.display.set_mode([self.max_x, self.max_y])
 
         self.dt = 10
-        pygame.time.delay(self.dt)
+        self.N = 5000
+        pygame.time.delay(100)
 
         self.veh = [(0, self.max_y//2)]
-        self.v = [Vehicle(0.001, self.veh[-1])]
+        self.v = [Vehicle(0.01, self.veh[-1])]
 
         self.running = True
         self._handle()
 
     def _handle(self):
-        while self.running:
+        for i in range(self.N):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    break
 
             self._refresh()
+            time.sleep(0.001/self.dt)
             pygame.display.flip()
         pygame.quit()
 
     def _struct(self):
         self.scr.fill((255, 255, 255))
-        pygame.draw.rect(self.scr, (100, 0, 255), pygame.Rect(-2, self.max_y//2, self.max_x + 4, 40), 2)
+        pygame.draw.rect(self.scr, (100, 0, 255), pygame.Rect(-2, self.max_y//2, self.max_x + 4, 20), 2)
 
     def _veh(self):
         if self.veh[-1][0] > 100:
             self.veh.append((0, self.max_y//2))
-            self.v.append(Vehicle(0.001, self.veh[-1]))
+            self.v.append(Vehicle(0.01, self.veh[-1]))
 
         for i in range(len(self.veh)):
             self.v[i].update(i, self.veh, self.dt)
@@ -53,25 +57,37 @@ class Vehicle:
         self.speed = speed
         self.x, self.y = coord[0], coord[1]
 
-        self.speed_pid = PID(0.5, 0, 0)
+        self.speed_pid = PID(0.5, 0.01, 0.3)
 
-        self.dist = 10
+        self.dist = 50
+
+        self.pos = []; self.err = []
 
     def get_model(self):
-        return pygame.Rect((self.x, 255, 40, 30))
+        return pygame.Rect((self.x, 255, 20, 10))
 
     def update(self, i, veh, dt):
         if i > 0:
-            output = self.speed_pid.compute(abs(veh[i-1][0] - veh[i][0]), self.dist, dt)/dt
-            if self.speed > 0:
-                self.speed += output
+            if self.speed >= 0 and self.speed < 0.5:
+                self.speed += self.speed_pid.compute(veh[i][0], veh[i-1][0] - self.dist, dt)/dt
+            elif self.speed > 0.5:
+                self.speed = 0.5
+            else:
+                self.speed = 0
+        else:
+            if self.speed >= 0 and self.speed < 0.5:
+                self.speed += self.speed_pid.compute(veh[i][0], 1000 - 50, dt)/dt
+            elif self.speed > 0.5:
+                self.speed = 0.5
             else:
                 self.speed = 0
 
-        else:
-            self.speed = 0.1*abs(cos(self.x/2*pi*1000))
+            print(self.x)
 
-        self.x += self.speed*dt
+
+        self.err.append(self.speed_pid.l_e[-1])
+        self.pos.append(self.x)
+        self.x += self.speed*dt*0.1
         veh[i] = (self.x, veh[i][1])
 
 
@@ -82,7 +98,7 @@ class PID:
         self.c = 0
 
     def compute(self, position, target, dt):
-        e = position - target
+        e = target - position
 
         self.l_e[self.c%2] = e
         self.c += 1
@@ -93,3 +109,5 @@ class PID:
 
 if __name__ == "__main__":
     t = Screen()
+    plt.plot(t.v[0].pos)
+    plt.show()
